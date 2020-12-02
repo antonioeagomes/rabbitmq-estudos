5dbbbc42a2b48220d1389825011dba092d1fbc10
Emmanuel Gomes <aeag@outlook.com>
2020-12-01 21:22:40 -0300

    First Commit in C#

---
+++ dotNet/Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var fabrica = new ConnectionFactory() { HostName = "localhost" };
+      
+      using (var conn = fabrica.CreateConnection())
+      using (var canal = conn.CreateModel())
+      {
+        canal.QueueDeclare(queue: "primeira.fila", 
+                durable: false, 
+                exclusive: false, 
+                autoDelete: false, 
+                arguments: null);
+
+        Console.WriteLine(" => Waiting for messages.");
+
+        var consumer = new EventingBasicConsumer(canal);
+        consumer.Received += (model, ea) =>
+        {
+          var body = ea.Body.ToArray();
+          var message = Encoding.UTF8.GetString(body);
+          Console.WriteLine($" [x] Received {message}");
+        };
+        canal.BasicConsume(queue: "primeira.fila", 
+                autoAck: true, 
+                consumer: consumer);
+
+        Console.WriteLine(" Press any to exit.");
+        Console.ReadLine();
+      }
+
+    }
+  }
+}
---
+++ dotNet/Publisher/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using (var conn = factory.CreateConnection())
+            using (var canal = conn.CreateModel())
+            {
+                canal.QueueDeclare(queue: "primeira.fila", 
+                    durable: false, 
+                    exclusive: false, 
+                    autoDelete: false, 
+                    arguments: null);
+
+                string message = "Hello world from RabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                canal.BasicPublish(exchange: "",
+                routingKey: "primeira.fila", // Bind
+                basicProperties: null,
+                body: body);
+
+                Console.WriteLine($" => Sent: {message}");
+            }    
+
+            Console.WriteLine($"Any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
